20f399355fe787d0688c1aa263ff83b49d6d522f
Roman_Kotovski <roman_kotovski@mail.ru>
2023-07-26 16:17:24 +0300

    update

--- 2.1-databases/work_with_database/phones/admin.py
+++ 2.1-databases/work_with_database/phones/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Phone
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'image', 'price', 'release_date',
+                    'lte_exists', 'slug']
+    list_filter = ['name', 'price']
+    prepopulated_fields = {'slug': ('name',)}
 
 # Register your models here.
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,30 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=50)
+    image = models.CharField(max_length=250)
+    price = models.IntegerField()
+    release_date = models.CharField(max_length=50)
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(null=False, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs) - models
+
+
+
+
+
+
+    # id = models.CharField(max_length=50, primary_key=True)
+    # name = models.CharField(max_length=50)
+    # price = models.PositiveIntegerField
+    # image = models.URLField(max_length=200)
+    # release_date = models.TimeField(max_length=50)
+    # lte_exists = models.BooleanField
+    # slug = models.SlugField(max_length=50, db_column=name, unique=True)
+
